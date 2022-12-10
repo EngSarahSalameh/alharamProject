98ee6ecea1133162480acb3ef96b3ca8341df0ca
Sarah Salameh <111677432+EngSarahSalameh@users.noreply.github.com>
2022-12-10 13:05:40 +0300

    Add files via upload

---
+++ App_Start/RouteConfig.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace alharamApp
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            //routes.IgnoreRoute("account/OnlySecritOperation010");
+            //routes.IgnoreRoute("account/adminOperations");
+
+
+            routes.MapRoute(
+             name: "defaults",
+             url: "{controller}/{action}/{id}",
+             defaults: new { controller = "alharam", action = "Index", id = UrlParameter.Optional });
+
+        }
+    }
+}
